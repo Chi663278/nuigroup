7c9268a90504e3f0a367618c06bba862920e5daf
Chi663278 <159125419+Chi663278@users.noreply.github.com>
2024-05-12 14:58:57 +0000

    [Update]views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: 'public/registrations',
+    sessions: 'public/sessions'
+  }
 
-  devise_for :admin, skip: [:registrations, :password], controllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
 
-  namespace :admin do
-    get 'dashboards', to: 'dashboards#index'
-    resources :users, only: [:destroy]
-  end
+
 end
