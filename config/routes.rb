0aa146329c051a01a9bed0fa079fb590dcb62ef0
Chi663278 <159125419+Chi663278@users.noreply.github.com>
2024-05-15 15:27:32 +0000

    [Update] routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,64 +1,45 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'follows/following'
-    get 'follows/follower'
-    get 'follows/create'
-    get 'follows/destroy'
-  end
-  namespace :public do
-    get 'comments/new'
-    get 'comments/create'
-    get 'comments/update'
-  end
-  namespace :public do
-    get 'favorites/index'
-    get 'favorites/create'
-    get 'favorites/update'
-  end
-  namespace :public do
-    get 'posts/new'
-    get 'posts/create'
-    get 'posts/update'
-  end
-  namespace :public do
-    get 'users/index'
-    get 'users/edit'
-    get 'users/update'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/timeline'
-  end
-  namespace :admin do
-    get 'favorites/user_favs'
-    get 'favorites/post_favs'
-    get 'favorites/update'
-  end
-  namespace :admin do
-    get 'follows/following'
-    get 'follows/follower'
-  end
-  namespace :admin do
-    get 'comments/user_comments'
-    get 'comments/post_comments'
-    get 'comments/update'
-  end
-  namespace :admin do
-    get 'posts/user_posts'
-    get 'posts/show'
-    get 'posts/update'
-  end
-  namespace :admin do
-    get 'users/index'
-  end
-  devise_for :users, skip: [:passwords], controllers: {
-    registrations: 'public/registrations',
-    sessions: 'public/sessions'
-  }
 
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
 
+  namespace :admin do
+    get '' => 'users#index'
+    get 'user_posts' => 'posts#user_posts', as: 'user_posts'
+    resources :posts, only: [:show, :update]
+    get 'user_comments' => 'comments#user_comments', as: 'user_comments'
+    get 'post_comments' => 'comments#post_comments', as: 'post_comments'
+    resources :comments, only: [:update]
+    get 'following/:user_id' => 'follows#following', as: 'following'
+    get 'follower/:user_id' => 'follows#follower', as: 'follower'
+    get 'user_favs/:user_id' => 'favorites#user_favs', as: 'user_favs'
+    get 'post_favs/:post_id' => 'favorites#post_favs', as: 'post_favs'
+    resources :favorites, only: [:destroy]
+  end
+
+
+
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: 'public/registrations',
+    sessions: 'public/sessions'
+  }
+
+  scope module: :public do
+    root 'homes#top'
+    get 'TL' => 'homes#timeline', as: 'timeline'
+    get ':screen_name' => 'users#index', as: 'user_timeline'
+    get 'user/edit' => 'users#edit'
+    patch 'user' => 'users#update'
+    resources :posts, only: [:new, :create]
+    patch 'posts/:id' => 'posts#update', as: 'delete_post'
+    resources :favorites, only: [:index, :create, :destroy]
+    resources :comments, only: [:new, :create]
+    patch 'comments/:id' => 'comments#update', as: 'delete_comment'
+    get 'following' => 'follows#following', as: 'following'
+    get 'follower' => 'follows#follower', as: 'follower'
+    resources :follows, only: [:create]
+    delete 'follow/:id' => 'follows#destroy', as: 'delete_follow'
+  end
 
 end
