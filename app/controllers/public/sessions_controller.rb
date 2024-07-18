d8ee3d956cc8aa248e9a0945c7e9b82375639c60
Chi663278 <159125419+Chi663278@users.noreply.github.com>
2024-07-18 16:07:33 +0000

    [Update] devise messages

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,6 @@
 
 class Public::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
-  before_action :reject_user, only: [:create]
 
   def after_sign_in_path_for(resource)
     timeline_path
@@ -14,17 +13,6 @@ class Public::SessionsController < Devise::SessionsController
 
   def configure_sign_in_params
     devise_parameter_sanitizer.permit(:sign_in, keys: [:screen_name])
-  end
-
-  protected
-  def reject_user
-    @user = User.find_by(screen_name: params[:user][:screen_name])
-    return if @user.nil?
-    return unless @user.valid_password?(params[:user][:password])
-    if @user.is_active == false
-        flash[:notice] = "ユーザーID: #{@user.screen_name}, ユーザー名: #{@user.name} は退会済みです。再度Sign upしてください。"
-        redirect_to new_user_registration_path
-    end
   end
 
 end
