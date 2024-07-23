b3b31072f24c20d6b0b250fe014936413f3562c8
Chi663278 <159125419+Chi663278@users.noreply.github.com>
2024-07-23 15:59:32 +0000

    [Update] Preview an image before upload

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,24 +1,45 @@
-if (document.URL.match(/new/)){
-  document.addEventListener('DOMContentLoaded', () => {
-    const createImageHTML = (blob) => {
-      const imageElement = document.getElementById('new-image');
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('class', 'new-img shadow mb-3')
-      blobImage.setAttribute('src', blob);
+document.addEventListener('DOMContentLoaded', () => {
+  const createImageHTML = (blob) => {
+    const modalElement = document.getElementById('modal');
+    const imageElement = document.getElementById('new-image');
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('class', 'new-img rounded shadow mb-3');
+    blobImage.setAttribute('src', blob);
 
-      imageElement.appendChild(blobImage);
-    };
+    imageElement.appendChild(blobImage);
+  };
 
-    document.getElementById('post_image').addEventListener('change', (e) => {
-      const imageContent = document.querySelector('img');
-      console.log(imageContent);
-      if (imageContent){
-        imageContent.remove();
-      }
+  document.getElementById('user_image').addEventListener('change', (e) => {
+    const imageContent = document.querySelector('img.new-img');
+    if (imageContent){
+      imageContent.remove();
+    }
 
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-      createImageHTML(blob);
-    });
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    createImageHTML(blob);
   });
-}
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const createImageHTML = (blob) => {
+    const modalElement = document.getElementById('modal');
+    const imageElement = document.getElementById('new-image');
+    const blobImage = document.createElement('img');
+    blobImage.setAttribute('class', 'new-img rounded shadow mb-3');
+    blobImage.setAttribute('src', blob);
+
+    imageElement.appendChild(blobImage);
+  };
+
+  document.getElementById('post_image').addEventListener('change', (e) => {
+    const imageContent = document.querySelector('img.new-img');
+    if (imageContent){
+      imageContent.remove();
+    }
+
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    createImageHTML(blob);
+  });
+});
